edc37b99d477046c7005ffe9d7255deb3722be9f
pocketberserker <pocketberserker@gmail.com>
2016-03-05 23:50:35 +0900

    Bump version to 1.0.3

--- src/Fake.Persimmon/AssemblyInfo.fs
+++ src/Fake.Persimmon/AssemblyInfo.fs
@@ -6,10 +6,10 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyProductAttribute("FAKE.Persimmon")>]
 [<assembly: GuidAttribute("547AB613-8059-4A80-BF6D-B3A19B220754")>]
 [<assembly: AssemblyDescriptionAttribute("FAKE extension for Persimmon")>]
-[<assembly: AssemblyVersionAttribute("1.0.2")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.2")>]
-[<assembly: AssemblyInformationalVersionAttribute("1.0.2")>]
+[<assembly: AssemblyVersionAttribute("1.0.3")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.3")>]
+[<assembly: AssemblyInformationalVersionAttribute("1.0.3")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.2"
+    let [<Literal>] Version = "1.0.3"
